53b0b02ac0dacb7d21d9fdbef00a2d5cf5217e8c
fcenciarelli <48931441+fcenciarelli@users.noreply.github.com>
2021-12-11 13:26:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -325,5 +325,7 @@ def transcript_analysis_from_sentence():
 if __name__ == '__main__':
     #you can execute all the functions written here
     #transcript_analysis_from_sentence()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
     app.run()
 
