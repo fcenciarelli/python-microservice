e2d6e4cbe1d991cbdafbc58e1bc95313f764c56c
fcenciarelli <48931441+fcenciarelli@users.noreply.github.com>
2021-12-11 15:05:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -327,5 +327,4 @@ if __name__ == '__main__':
     #transcript_analysis_from_sentence()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
-    app.run()
 
