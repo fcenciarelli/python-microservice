1f4d80a289a946e474fb2144e7381975c1729eb5
fcenciarelli <fracenciarelli2000@gmail.com>
2021-12-13 23:01:33 +0100

    commit

--- app.py
+++ app.py
@@ -23,12 +23,11 @@ from google.cloud import storage
 # Declearing that the app is using Flask
 app = Flask(__name__)
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/ServiceKey_GoogleCloud.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = app.config['/ServiceKey_GoogleCloud.json']
 storage_client = storage.Client()
 
 
 def upload_to_bucket(blob_name, file_path, bucket_name):
-    storage_client = storage.Client()
     try:
         bucket = storage_client.get_bucket(bucket_name)
         blob = bucket.blob(blob_name)
