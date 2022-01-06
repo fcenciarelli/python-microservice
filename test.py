078b484a7b0e84c1e1a4121e320055b11c33b621
Teo56 <mp1319@ic.ac.uk>
2022-01-06 12:27:37 +0100

    updated list remove

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 words_remove = []
+
 with open('remove list.txt', 'r') as f:
     for line in f:
+        #print(line)
         b = line.strip()
         a= b.split('\n')
         words_remove.append(a[0])
