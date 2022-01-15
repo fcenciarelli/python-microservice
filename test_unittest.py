bc141d47c493144a7952209adde2e39e7c0a7c04
Teo56 <77128181+Teo56@users.noreply.github.com>
2022-01-15 17:07:20 +0100

    Update test_unittest.py

--- test_unittest.py
+++ test_unittest.py
@@ -24,14 +24,9 @@ class TestSum(unittest.TestCase):
     # test the retrieve_transcripts_youtube function by inputting a wrong videoID and checking whether the output is the proper one 
     # the output is used as an "emergency" srt
     def test_retrieve_transcripts_youtube(self):
-        #random video id --> expect usual str
-        #print(retrieve_transcripts_youtube("hjjkak"))
-        #print("\n\n\n")
-        srt = retrieve_transcripts_youtube("fhsjd")
-        #srt = [{'text': 'this is where you come and punish', 'start': 0.0, 'duration': 3.99}, {'text': 'yourself for fun a rather for your', 'start': 1.68, 'duration': 4.52}, {'text': 'health', 'start': 3.99, 'duration': 2.21}, {'text': 'here you get rubbed down shaken up', 'start': 7.88, 'duration': 4.27}, {'text': 'crumbled and pushed around for a price', 'start': 10.38, 'duration': 4.049}, {'text': 'and a purpose if you want to turn fat', 'start': 12.15, 'duration': 5.219}, {'text': "and fled into nice hard muscle there's", 'start': 14.429, 'duration': 4.081}, {'text': 'central heating and wall-to-wall', 'start': 17.369, 'duration': 3.481}, {'text': 'carpeting to soften the blows and all', 'start': 18.51, 'duration': 4.679}, {'text': 'kinds of mechanical wonders to waste the', 'start': 20.85, 'duration': 5.089}, {'text': 'way your waste', 'start': 23.189, 'duration': 2.75}, {'text': 'once you join the club you can get down', 'start': 36.5, 'duration': 3.78}, {'text': 'to a workout when you like for as long', 'start': 38.63, 'duration': 3.75}]
+        srt = retrieve_transcripts_youtube("fhsjd") # retrieves transcript from the function
         sentence = json.dumps(srt[0]).split('"')[3]
-        print(sentence)
-        self.assertEqual(sentence, "this is where you come and punish", "Test Failed")
+        self.assertEqual(sentence, "this is where you come and punish", "Test Failed") # asserts the first sentence of the transcript is the expected one
 
 if __name__ == '__main__':
     unittest.main()
